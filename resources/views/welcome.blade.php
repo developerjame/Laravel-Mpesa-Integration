6ab271f07d3e41c6fe84a2bff871df76f9228239
developerjame <119099522+developerjame@users.noreply.github.com>
2024-01-31 22:18:55 +0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,6 +54,7 @@
 
         </div>
     </center>
+   
     <script>
         document.getElementById('getAccessToken').addEventListener('click', (event) => {
             event.preventDefault()
@@ -61,6 +62,7 @@
             axios.post('/get-token', {})
             .then((response) => {
                 console.log(response.data);
+                document.getElementById('access_token').innerHTML = response.data.access_token
             })
             .catch((error) => {
                 console.log(error);
